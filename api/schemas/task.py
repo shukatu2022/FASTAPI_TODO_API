30cdd1cbef8987f4b764666538d4c081bbef8a16
user <user@email.com>
2026-02-27 10:24:29 +0900

    すべてのリクエストとレスポンスの定義

---
+++ api/schemas/task.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, Field
+
+
+class TaskBase(BaseModel):
+    title: str | None = Field(None, examples=["クリーニングを取りに行く"])
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskCreateResponse(TaskBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Task(TaskBase):
+    id: int
+    done: bool = Field(False, description="完了フラグ")
+
+    class Config:
+        orm_mode = True
