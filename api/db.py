237aa071327a049245d674dc5242f372a086875d
user <user@email.com>
2026-04-22 21:21:51 +0900

    render で deploy するために環境変数対応に変更

--- api/db.py
+++ api/db.py
@@ -1,19 +1,38 @@
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-ASYNC_DB_URL = "mysql+aiomysql://root@db:3306/demo?charset=utf8"
+ASYNC_DB_URL = os.getenv("DATABASE_URL", "mysql+aiomysql://root@db:3306/demo?charset=utf8")
 
 async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
 async_session = sessionmaker(
-                    autocommit=False,
-                    autoflush=False,
-                    bind=async_engine,
-                    class_=AsyncSession,
-                    )
+    autocommit=False,
+    autoflush=False,
+    bind=async_engine,
+    class_=AsyncSession,
+)
 
 Base = declarative_base()
-
 
 async def get_db():
     async with async_session() as session:
         yield session
+
+
+# 以下は mysql 用
+# ASYNC_DB_URL = "mysql+aiomysql://root@db:3306/demo?charset=utf8"
+
+# async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
+# async_session = sessionmaker(
+#                     autocommit=False,
+#                     autoflush=False,
+#                     bind=async_engine,
+#                     class_=AsyncSession,
+#                     )
+
+# Base = declarative_base()
+
+
+# async def get_db():
+#     async with async_session() as session:
+#         yield session
