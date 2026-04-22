475cbcb10a1ce5305465b3c25fdf44dd22975d35
user <user@email.com>
2026-04-22 20:55:18 +0900

    from api.models.task から from models.task

--- api/migrate_db.py
+++ api/migrate_db.py
@@ -1,12 +1,13 @@
 from sqlalchemy import create_engine
 
-from api.models.task import Base
+from models.task import Base
 
 
 DB_URL = "mysql+pymysql://root@db:3306/demo?charset=utf8"
 engine = create_engine(DB_URL, echo=True)
 
 
+# 毎回データを消去
 def reset_database():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
