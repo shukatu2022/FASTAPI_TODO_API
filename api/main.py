5122d0bcd33221c6b9382dc32c64cc509ffd800e
user <user@email.com>
2026-04-22 21:53:04 +0900

    render で deploy するコード

--- api/main.py
+++ api/main.py
@@ -1,10 +1,20 @@
+# render で deploy するためのコード
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
 from api.routers import task, done
+from api.db import async_engine, Base
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(task.router)
 app.include_router(done.router)
 
@@ -14,3 +24,22 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/")
 async def read_index():
     return FileResponse("static/index.html")
+
+
+# ローカルで動かすためのコード
+# from fastapi import FastAPI
+# from fastapi.staticfiles import StaticFiles
+# from fastapi.responses import FileResponse
+
+# from api.routers import task, done
+
+# app = FastAPI()
+# app.include_router(task.router)
+# app.include_router(done.router)
+
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+# @app.get("/")
+# async def read_index():
+#     return FileResponse("static/index.html")
