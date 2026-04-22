db9ec64d31f984dbfde730c72432be816b67e5fd
user <user@email.com>
2026-04-22 21:33:36 +0900

    sqlite 用に変更

--- api/db.py
+++ api/db.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-ASYNC_DB_URL = os.getenv("DATABASE_URL", "mysql+aiomysql://root@db:3306/demo?charset=utf8")
+ASYNC_DB_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./demo.db")
 
 async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
 async_session = sessionmaker(
@@ -13,6 +13,7 @@ async_session = sessionmaker(
 )
 
 Base = declarative_base()
+
 
 async def get_db():
     async with async_session() as session:
